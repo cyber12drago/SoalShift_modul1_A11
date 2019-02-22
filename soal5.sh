b1c86581cb8ab6f56a1bef3e9898fbeca9a8f087
diondevara <42793740+diondevara@users.noreply.github.com>
2019-02-22 17:27:05 +0700

    Update soal5.sh

--- soal5.sh
+++ soal5.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cat /var/log/syslog | awk '(/!sudo/ || /cron/ && /CRON/) && (NF<13){print}' >> /home/diondevara/modul1/soal_5.log
+cat /var/log/syslog | awk '(!/sudo/ || /cron/ && /CRON/) && (NF<13){print}' >> /home/diondevara/modul1/soal_5.log
 
